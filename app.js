8755ded47447c64e044da4afebb78886c1a4e308
Dannie Schumaker <draeschumaker@gmail.com>
2020-02-19 18:01:07 -0800

    add yelp endpoint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const weather = require('./data/darksky.js');
+// const weather = require('./data/darksky.js');
 const app = express();
 const request = require('superagent');
 // const cors = require('cors');
@@ -12,7 +12,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 let lat;
 let lng;
 
-app.get('/location', async(req, res, next) => {
+app.get('/location', async (req, res, next) => {
     try {
         // get location from query params
         const location = req.query.search;
@@ -22,35 +22,65 @@ app.get('/location', async(req, res, next) => {
         const cityData = await request.get(URL);
         // store data for first result
         const firstResult = cityData.body[0];
-        
         // update global state of lat and lng
         lat = firstResult.lat;
         lng = firstResult.lon;
-    
+
         res.json({
             formatted_query: firstResult.display_name,
             latitude: lat,
             longitude: lng,
         });
-    // if this breaks, catch error and pass to next 
+        // if this breaks, catch error and pass to next 
     } catch (err) {
         next(err);
     }
 });
 
-const getWeatherData = (lat, lng) => {
-    return weather.daily.data.map(forecast => {
+const getWeatherData = async (lat, lng, next) => {
+    try {
+        const URL = `https://api.darksky.net/forecast/ea00a4754ac9af00b3d8fa931923f3ec/${lat},${lng}?key=${process.env.DARKSKY_API_KEY}`;
+
+        const weather = await request.get(URL);
+
+        return weather.daily.data.map(forecast => {
+            return {
+                forecast: forecast.summary,
+                time: new Date(forecast.time * 1000)
+            };
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+app.get('/weather', async (req, res) => {
+    const localWeather = await getWeatherData(lat, lng);
+
+    res.json(localWeather);
+});
+
+
+const getYelpData = async (lat, lng) => {
+    const URL = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${lat}&longitude=${lng}`;
+    const yelp = await request.get(URL).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`);
+
+    return yelp.body.businesses.map(business => {
         return {
-            forecast: forecast.summary,
-            time: new Date(forecast.time * 1000)
+            rating: business.rating,
+            name: business.alias,
+            price: business.price,
+            url: business.url,
+            image: business.image_url
         };
     });
 };
 
-app.get('/weather', (req, res) => {
-    const portlandWeather = getWeatherData(lat, lng);
 
-    res.json(portlandWeather);
+app.get('/yelp', async (req, res) => {
+    const localReviews = await getYelpData(lat, lng);
+
+    res.json(localReviews);
 });
 
 app.get('*', (req, res) => res.send('404!'));
